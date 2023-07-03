2c5931d51ef2daac583b49d370b061f8870edb81
mBonny87 <36232591+mBonny87@users.noreply.github.com>
2023-07-03 20:26:49 +0200

    doubly

--- pkg/linkedlist/DoublyLinkedList.go
+++ pkg/linkedlist/DoublyLinkedList.go
@@ -1,5 +1,32 @@
 package linkedlist
 
-// type DoublyLinkedList struct {
-// 	head Node
-// }
+import "fmt"
+
+type DoublyLinkedList struct {
+	head *Node
+	tail *Node
+}
+
+func (dll *DoublyLinkedList) InsertAtBeginning(v int) {
+	if dll.head == nil {
+		dll.head = &Node{value: v, next: nil, prev: nil}
+		dll.tail = &Node{value: v, next: nil, prev: nil}
+		return
+	}
+	temp := dll.head
+	new := &Node{value: v, next: temp, prev: nil}
+	dll.head = new
+
+	for temp.next != nil {
+		temp = temp.next
+	}
+	dll.tail = temp
+}
+
+func (dll *DoublyLinkedList) Print() {
+	curr := dll.head
+	for curr != nil {
+		fmt.Println("Value:", curr.value)
+		curr = curr.next
+	}
+}
