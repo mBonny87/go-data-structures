8a08288caf75d01f8dc0b2dd1923e46d232d4ac7
mBonny87 <36232591+mBonny87@users.noreply.github.com>
2023-07-11 00:19:47 +0200

    add shift method and some tests

--- pkg/linkedlist/circular_linked_list.go
+++ pkg/linkedlist/circular_linked_list.go
@@ -43,6 +43,35 @@ func (cll *CircularLinkedList) Shift(v int) {
 	cll.Head = curr.Next
 }
 
+func (cll *CircularLinkedList) Pop() bool {
+	if cll.Head == nil {
+		return false
+	}
+	if cll.Head.Next == cll.Head {
+		cll.Head = nil
+		return false
+	}
+
+	curr := cll.Head
+	for curr.Next.Next != cll.Head {
+		curr = curr.Next
+	}
+	curr.Next = cll.Head
+	return true
+}
+
+func (cll *CircularLinkedList) Unshift() {
+
+}
+
+// Exercise 2: Implement a method Search(value int) *Node that searches for the first occurrence of the given value in the circular linked list and returns the corresponding node. If the value is not found, the method should return nil.
+
+// Exercise 3: Implement a method Size() int that returns the number of nodes in the circular linked list.
+
+// Exercise 4: Implement a method ToArray() []int that converts the circular linked list to an array and returns it. The array should contain the values of the nodes in the same order as they appear in the circular linked list.
+
+// Exercise 5: Implement a method Reverse() that reverses the order of the nodes in the circular linked list.
+
 func (cll *CircularLinkedList) Print() {
 	curr := cll.Head
 	for curr.Next != cll.Head {
--- pkg/linkedlist/circular_linked_list_test.go
+++ pkg/linkedlist/circular_linked_list_test.go
@@ -11,6 +11,7 @@ func TestCircularLinkedList(t *testing.T) {
 	cll.Push(20)
 	cll.Push(25)
 
+	// Check the head
 	if cll.Head.Value != 10 {
 		t.Errorf("Expected 10 as the head but got %v", cll.Head.Value)
 	}
@@ -20,12 +21,26 @@ func TestCircularLinkedList(t *testing.T) {
 		curr = curr.Next
 	}
 
+	// Check the tail
 	if curr.Value != 25 {
 		t.Errorf("Expected 25 as the tail but got %v", curr.Value)
 	}
 
+	// Chech head after shift
 	cll.Shift(5)
 	if cll.Head.Value != 5 {
 		t.Errorf("Expected 5 as the head but got %v", cll.Head.Value)
 	}
+
+	// check tail after pop
+	if cll.Pop() {
+		curr = cll.Head
+		for curr != nil && curr.Next != cll.Head {
+			curr = curr.Next
+		}
+		if curr.Value != 20 {
+			t.Errorf("Expected 20 as the tail but got %v", curr.Value)
+		}
+	}
+
 }
