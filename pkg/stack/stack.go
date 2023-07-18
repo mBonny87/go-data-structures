fe3f397c0dd0331281d76fcbb3ea7ad19b381ce9
mBonny87 <36232591+mBonny87@users.noreply.github.com>
2023-07-18 23:57:43 +0200

    Implementation of a stack

--- pkg/stack/stack.go
+++ pkg/stack/stack.go
@@ -1,19 +1,72 @@
 package stack
 
+import "fmt"
+
 type Stack struct {
 	Top *Node
+}
+
+func (s *Stack) Push(v int) {
+	if s.Top == nil {
+		s.Top = &Node{Value: v, Next: nil}
+		return
+	}
+	curr := s.Top
+	s.Top = &Node{Value: v, Next: curr}
+}
+
+func (s *Stack) Pop() {
+	if s.Top == nil {
+		// already empty
+		return
+	}
+	if s.Top.Next == nil {
+		//one element
+		s.Top = nil
+		return
+	}
+	s.Top = s.Top.Next
+}
+
+func (s *Stack) Print() {
+	if s.Top == nil {
+		return
+	}
+	curr := s.Top
+	for curr.Next != nil {
+		fmt.Println(curr.Value)
+		curr = curr.Next
+	}
+	//print last value
+	fmt.Println(curr.Value)
+}
+
+func (s *Stack) Peek() int {
+	if s.Top == nil {
+		return 0
+	}
+	return s.Top.Value
+}
+
+func (s *Stack) Size() int {
+	if s.Top == nil {
+		return 0
+	}
+	i := 1
+	curr := s.Top
+	for curr.Next != nil {
+		i = i + 1
+		curr = curr.Next
+	}
+	return i
+}
+
+func (s *Stack) Clear() {
+	s.Top = nil
 }
 
 // # Exercise 1
 
 // Implement the following operations for a Stack:
 
-// Push: Implement a method Push(value int) that adds a new element with the given value to the top of the stack.
-// Pop: Implement a method Pop() int that removes and returns the element from the top of the stack.
-// Peek: Implement a method Peek() int that returns the element at the top of the stack without removing it.
-// IsEmpty: Implement a method IsEmpty() bool that checks if the stack is empty and returns true if it is, false otherwise.
-// Exercise 2: Implement a method Size() int that returns the number of elements currently in the stack.
-
 // Exercise 3: Implement a method Clear() that removes all elements from the stack, effectively emptying it.
-
-// Exercise 4: Implement a method Print() that prints the elements of the stack in the order they would be popped, starting from the top.
--- pkg/stack/stack_test.go
+++ pkg/stack/stack_test.go
@@ -1,7 +1,37 @@
 package stack
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestStack(t *testing.T) {
+	s := Stack{}
 
+	s.Push(5)
+	s.Push(10)
+	s.Push(15)
+	s.Push(20)
+
+	s.Pop()
+	s.Pop()
+	fmt.Printf("peeked %v", s.Peek())
+	fmt.Println()
+
+	if s.Size() != 2 {
+		fmt.Printf("Expected 2 but found %v", s.Peek())
+	}
+
+	// s.Pop()
+	// s.Pop()
+
+	// if s.Size() != 0 {
+	// 	fmt.Printf("Expected 0 but found %v", s.Peek())
+	// }
+
+	s.Print()
+	fmt.Println("Clearing the Stack...")
+	s.Clear()
+	fmt.Println("Printing to check if empty...")
+	s.Print()
 }
