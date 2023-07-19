c594056c44c07a2d33ab677c5f312c0c3c8b1cfc
mBonny87 <36232591+mBonny87@users.noreply.github.com>
2023-07-20 00:48:50 +0200

    array implementation of a queue

--- pkg/queue/array_queue.go
+++ pkg/queue/array_queue.go
@@ -1,1 +1,72 @@
 package queue
+
+import "fmt"
+
+//this is a case with an array queue of length 5
+
+func Enqueue(v int, queue *[5]int) [5]int {
+	// full
+	this := (*queue)
+	if this[len(this)-1] != 0 {
+		return this
+	}
+	for i, value := range this {
+		if value != 0 {
+			continue
+		} else {
+			this[i] = v
+			break
+		}
+	}
+	(*queue) = this
+	return (*queue)
+}
+
+func Dequeue(queue *[5]int) [5]int {
+	// check if there's some data
+	this := (*queue)
+	if this[0] == 0 {
+		return this
+	}
+
+	//move right indexes to left
+	for i := range this {
+		if i+1 == len(this) {
+			this[i] = 0
+			break
+		}
+		this[i] = this[i+1]
+	}
+	(*queue) = this
+	return (*queue)
+}
+
+func Peek(queue *[5]int) int {
+	return (*queue)[0]
+}
+
+func IsEmpty(queue *[5]int) bool {
+	return (*queue)[0] == 0
+}
+
+func Size(queue *[5]int) int {
+	for i, value := range *queue {
+		if value == 0 {
+			return i
+		}
+	}
+	return len(queue)
+}
+
+func Clear(queue *[5]int) [5]int {
+	for i := range *queue {
+		(*queue)[i] = 0
+	}
+	return (*queue)
+}
+
+func Print(queue *[5]int) {
+	for _, value := range *queue {
+		fmt.Println(value)
+	}
+}
--- pkg/queue/array_queue_test.go
+++ pkg/queue/array_queue_test.go
@@ -1,1 +1,45 @@
 package queue
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestArrayQueue(t *testing.T) {
+	queue := &[5]int{}
+	//Enqueue
+	Enqueue(6, queue)
+	Enqueue(4, queue)
+	Enqueue(5, queue)
+	Enqueue(10, queue)
+	Enqueue(10, queue)
+	//This is skipper
+	Enqueue(10, queue)
+
+	// Dequeue
+	Dequeue(queue)
+
+	fmt.Printf("Peeked: %v", Peek(queue))
+	fmt.Println()
+	fmt.Printf("Empty: %v", IsEmpty(queue))
+	fmt.Println()
+	fmt.Printf("Size: %v", Size(queue))
+	fmt.Println()
+
+	fmt.Println("Current queue:")
+	Print(queue)
+
+	//Clear
+
+	fmt.Println("Clearing...")
+	Clear(queue)
+	fmt.Printf("Peeked: %v", Peek(queue))
+	fmt.Println()
+	fmt.Printf("Empty: %v", IsEmpty(queue))
+	fmt.Println()
+	fmt.Printf("Size: %v", Size(queue))
+	fmt.Println()
+
+	fmt.Println("Current queue:")
+	Print(queue)
+}
