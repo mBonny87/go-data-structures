04833da2c12babe03c545ff7d62003f93f20aebf
mBonny87 <36232591+mBonny87@users.noreply.github.com>
2023-07-21 01:25:53 +0200

    implemented circular queue

--- pkg/queue/circular_queue.go
+++ pkg/queue/circular_queue.go
@@ -1,27 +1,79 @@
-// package queue
+package queue
 
-// type CircularQueue struct {
-// 	Front *Node
-// 	Rear  *Node
-// }
+import "fmt"
 
-// func (cq *CircularQueue) Enqueue(v int) {
-// }
+// That's dynamic, not fixed size
+type CircularQueue struct {
+	Front *Node
+	Rear  *Node
+}
 
-// func (cq *CircularQueue) Dequeue() {
-// }
+func (cq *CircularQueue) Enqueue(v int) {
+	if cq.Front == nil {
+		cq.Front = &Node{Value: v, Next: nil}
+		cq.Rear = cq.Front
+		cq.Front.Next = cq.Rear
+		cq.Rear.Next = cq.Front
+		return
+	}
+	curr := cq.Front
+	for curr.Next != cq.Front {
+		curr = curr.Next
+	}
+	curr.Next = &Node{Value: v, Next: cq.Front}
+	cq.Rear = curr.Next
+}
 
-// func (cq *CircularQueue) Peek() int {
-// }
+func (cq *CircularQueue) Dequeue() {
+	if cq.Front == nil {
+		//no nodes
+		return
+	}
+	if cq.Front.Next == nil {
+		// one node, return empty
+		return
+	}
+	cq.Front = cq.Front.Next
+	cq.Rear.Next = cq.Front
+}
 
-// func (cq *CircularQueue) IsEmpty() bool {
-// }
+func (cq *CircularQueue) Peek() int {
+	if cq.Front == nil {
+		return 0
+	}
+	return cq.Front.Value
+}
 
-// func (cq *CircularQueue) Size() int {
-// }
+func (cq *CircularQueue) IsEmpty() bool {
+	return cq.Front == nil
+}
 
-// func (cq *CircularQueue) Clear() {
-// }
+func (cq *CircularQueue) Size() int {
+	if cq.Front == nil {
+		return 0
+	}
+	i := 1
+	curr := cq.Front
+	for curr != cq.Rear {
+		i++
+		curr = curr.Next
+	}
+	return i
+}
 
-// func (cq *CircularQueue) Print() {
-// }
+func (cq *CircularQueue) Clear() {
+	cq.Front = nil
+	cq.Rear = nil
+}
+
+func (cq *CircularQueue) Print() {
+	if cq.Front == nil {
+		return
+	}
+	curr := cq.Front
+	for curr.Next != cq.Front {
+		fmt.Println(curr.Value)
+		curr = curr.Next
+	}
+	fmt.Println(curr.Value)
+}
--- pkg/queue/circular_queue_test.go
+++ pkg/queue/circular_queue_test.go
@@ -1,1 +1,28 @@
 package queue
+
+import (
+	"testing"
+)
+
+func TestCircularQueue(t *testing.T) {
+	cq := CircularQueue{}
+
+	cq.Enqueue(5)
+	cq.Enqueue(10)
+	cq.Enqueue(15)
+	cq.Enqueue(20)
+	cq.Dequeue()
+	cq.Dequeue()
+	cq.Dequeue()
+	if cq.Peek() != 20 {
+		t.Errorf("Expected 20 but found %v", cq.Peek())
+	}
+	if cq.IsEmpty() {
+		t.Errorf("Expected false but found %v", cq.IsEmpty())
+	}
+	if cq.Size() != 1 {
+		t.Errorf("Expected 1 but found %v", cq.Size())
+	}
+
+	cq.Print() // 5 15
+}
